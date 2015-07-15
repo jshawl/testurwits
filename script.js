9809cebf89560ed265815d3c9f5ed932d79d3e43
Anand Moses <moses.adg@gmail.com>
2015-07-15 06:38:42 -0400

    MVP 3

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 $(document).ready(function(){
    $('#questionID1').on('click', function() {
    var result =($('input[name=qRadio1]:checked', '#questionID1').val());
-   if(result=="Answer1"){
+   if(result=="Answer2"){
     $("#answerResult1").val("Correct")
     score ++;
     }
@@ -62,7 +62,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('#questionID2').on('click', function() {
     var result =($('input[name=qRadio2]:checked', '#questionID2').val());
-    if(result=="Answer4"){
+    if(result=="Answer1"){
     $("#answerResult2").val("Correct")
     score++;
     }
@@ -71,11 +71,138 @@ $(document).ready(function(){
     }
     $("#answerFinal1").val("Your score is:" +score)
    question ++;
+     $("#questionID3").show();
    $('#questionID2').replaceWith($(this).next('#questionID3'));
     });
 
 });
 
+$(document).ready(function(){
+    $('#questionID3').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID3').val());
+    if(result=="Answer4"){
+    $("#answerResult3").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult3").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID4").show();
+   $('#questionID3').replaceWith($(this).next('#questionID4'));
+    });
+
+});
+
+$(document).ready(function(){
+    $('#questionID4').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID4').val());
+    if(result=="Answer4"){
+    $("#answerResult4").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult4").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID5").show();
+   $('#questionID4').replaceWith($(this).next('#questionID5'));
+    });
+
+});
+
+$(document).ready(function(){
+    $('#questionID5').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID5').val());
+    if(result=="Answer4"){
+    $("#answerResult5").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult5").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID6").show();
+   $('#questionID5').replaceWith($(this).next('#questionID6'));
+    });
+
+});
+
+$(document).ready(function(){
+    $('#questionID6').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID6').val());
+    if(result=="Answer4"){
+    $("#answerResult6").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult6").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID7").show();
+   $('#questionID6').replaceWith($(this).next('#questionID7'));
+    });
+
+});
+
+
+$(document).ready(function(){
+    $('#questionID7').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID7').val());
+    if(result=="Answer4"){
+    $("#answerResult7").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult7").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID8").show();
+   $('#questionID7').replaceWith($(this).next('#questionID8'));
+    });
+
+});
+
+$(document).ready(function(){
+    $('#questionID8').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID8').val());
+    if(result=="Answer4"){
+    $("#answerResult8").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult8").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID9").show();
+   $('#questionID8').replaceWith($(this).next('#questionID9'));
+    });
+
+});
+
+$(document).ready(function(){
+    $('#questionID9').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID9').val());
+    if(result=="Answer4"){
+    $("#answerResult9").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult9").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
+   question ++;
+    $("#questionID10").show();
+   $('#questionID9').replaceWith($(this).next('#questionID10'));
+    });
+
+});
 
 $(document).ready(function(){
 $(".scroll").click(function(event){
