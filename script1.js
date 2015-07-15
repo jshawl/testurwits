51841d2d5a221d9b91dc78e59df0f3fda1345acb
Anand Moses <moses.adg@gmail.com>
2015-07-15 06:06:01 -0400

    MVP 1

--- script1.js
+++ script1.js
@@ -6,25 +6,37 @@ var name =0;
 var counter=0;
 
 $(document).ready(function(){
+$("#questionID2").hide();
+$("#questionID3").hide();
+$("#questionID4").hide();
+$("#questionID5").hide();
+$("#questionID6").hide();
+$("#questionID7").hide();
+$("#questionID8").hide();
+$("#questionID9").hide();
+$("#questionID10").hide();
+});
 
-for (var i = 0; i< 1; i++){
+$(document).ready(function(){
 
+    for (var i = 0; i< 1; i++){
+        $("#play1a").append(localStorage.key(i));
+        $("#play1b").append(localStorage.getItem(localStorage.key(i)));
+        i++;
 
-  $("#play1a").append(localStorage.key(i));
-  $("#play1b").append(localStorage.getItem(localStorage.key(i)));
-i++;
-
-  $("#play2a").append(localStorage.key(i));
-  $("#play2b").append(localStorage.getItem(localStorage.key(i)));
-  i++;
-  $("#play3a").append(localStorage.key(i));
-  $("#play3b").append(localStorage.getItem(localStorage.key(i)));
-}
-for ( var i = 0; i < 5; i++)   {
+        $("#play2a").append(localStorage.key(i));
+        $("#play2b").append(localStorage.getItem(localStorage.key(i)));
+        i++;
+        $("#play3a").append(localStorage.key(i));
+        $("#play3b").append(localStorage.getItem(localStorage.key(i)));
+      }
+      for ( var i = 0; i < 5; i++)   {
       console.log(localStorage.key(i));
       console.log(localStorage.getItem(localStorage.key(i)));
-    }
+      }
 });
+
+
 
 $(document).ready(function(){
    $('#questionID1').on('click', function() {
@@ -37,39 +49,54 @@ $(document).ready(function(){
     $("#answerResult1").val("Incorrect")
      }
      $("#answerFinal1").val("Your score is:" +score)
-     //$("input:radio").attr('disabled',true);
-    //  $(this).attr('disabled','disabled');
-    $("input[name=qRadio1]").attr('disabled', true);
-    // $(this).attr('disabled', 'disabled');
+
     question ++;
+    $("#questionID2").show();
+    $('#questionID1').replaceWith($(this).next('#questionID2'));
+    });
+
 });
 
 
-$('#questionID2').on('click', function() {
-var result =($('input[name=qRadio2]:checked', '#questionID2').val());
-if(result=="Answer4"){
- $("#answerResult2").val("Correct")
- score++;
- }
- else {
- $("#answerResult2").val("Incorrect")
-  }
-   $("#answerFinal1").val("Your score is:" +score)
-   $("input[name=qRadio2]").attr('disabled', true);
+
+$(document).ready(function(){
+    $('#questionID2').on('click', function() {
+    var result =($('input[name=qRadio2]:checked', '#questionID2').val());
+    if(result=="Answer4"){
+    $("#answerResult2").val("Correct")
+    score++;
+    }
+    else {
+    $("#answerResult2").val("Incorrect")
+    }
+    $("#answerFinal1").val("Your score is:" +score)
    question ++;
-    // $(this).attr('disabled','disabled');
-    // $("input:radio").attr('disabled',true);
+   $('#questionID2').replaceWith($(this).next('#questionID3'));
+    });
+
 });
+
+
+$(document).ready(function(){
 $(".scroll").click(function(event){
+   name = prompt("Enter name")
    event.preventDefault();
    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-   name = prompt("Enter name")
-   //localStorage.clear();//to clear localStorage
+
  });
 
 
 $('.giveup').on('click', function(event) {
   event.preventDefault();
+  $("#questionID2").show();
+  $("#questionID3").show();
+  $("#questionID4").show();
+  $("#questionID5").show();
+  $("#questionID6").show();
+  $("#questionID7").show();
+  $("#questionID8").show();
+  $("#questionID9").show();
+  $("#questionID10").show();
 
   $("#answerResult1").val("Correct Answer!!")
   $("#answerResult2").val("Correct Answer!!")
@@ -89,6 +116,4 @@ $('.donegame').on('click', function() {
   localStorage.setItem(name, score);
 
 });
-
-
 });
